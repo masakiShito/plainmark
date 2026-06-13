9911f52b57652e1ab19728825ec8d0bb5dfef8f0
師藤真基 <98877813+masakiShito@users.noreply.github.com>
2026-06-14 08:44:07 +0900

    Load GitHub Works Sync admin module

--- theme/functions.php
+++ theme/functions.php
@@ -24,6 +24,7 @@ require_once PLAINMARK_DIR . '/inc/custom-post-types.php';
 require_once PLAINMARK_DIR . '/inc/toc-functions.php';
 require_once PLAINMARK_DIR . '/inc/admin/article-settings.php';
 require_once PLAINMARK_DIR . '/inc/admin/work-settings.php';
+require_once PLAINMARK_DIR . '/inc/admin/github-works-sync.php';
 require_once PLAINMARK_DIR . '/inc/admin/sample-works.php';
 require_once PLAINMARK_DIR . '/inc/enqueue.php';
 require_once PLAINMARK_DIR . '/inc/customizer.php';
