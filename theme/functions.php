6c9d7bed30b3671eda87bafe5bfe6d4318e284d0
師藤真基 <98877813+masakiShito@users.noreply.github.com>
2026-06-16 16:01:15 +0900

    Load article inventory admin page

--- theme/functions.php
+++ theme/functions.php
@@ -23,6 +23,7 @@ require_once PLAINMARK_DIR . '/inc/setup.php';
 require_once PLAINMARK_DIR . '/inc/custom-post-types.php';
 require_once PLAINMARK_DIR . '/inc/toc-functions.php';
 require_once PLAINMARK_DIR . '/inc/admin/article-settings.php';
+require_once PLAINMARK_DIR . '/inc/admin/article-inventory.php';
 require_once PLAINMARK_DIR . '/inc/admin/work-settings.php';
 require_once PLAINMARK_DIR . '/inc/admin/github-works-sync.php';
 require_once PLAINMARK_DIR . '/inc/admin/sample-works.php';
