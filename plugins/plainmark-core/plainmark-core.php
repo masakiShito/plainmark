683cd7ecf5f3ec5fea9d8668aaeb38cfb5fb6b29
師藤真基 <98877813+masakiShito@users.noreply.github.com>
2026-06-17 17:12:33 +0900

    Add bundled Plainmark core plugin

---
+++ plugins/plainmark-core/plainmark-core.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name: Plainmark Core
+ * Plugin URI: https://github.com/masakiShito/plainmark
+ * Description: Core data model and editorial governance features for the Plainmark theme.
+ * Version: 0.1.0
+ * Author: plainmark
+ * Text Domain: plainmark
+ *
+ * @package plainmark-core
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! defined( 'PLAINMARK_CORE_VERSION' ) ) {
+	define( 'PLAINMARK_CORE_VERSION', '0.1.0' );
+}
+
+if ( ! defined( 'PLAINMARK_CORE_DIR' ) ) {
+	define( 'PLAINMARK_CORE_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'PLAINMARK_CORE_URI' ) ) {
+	define( 'PLAINMARK_CORE_URI', plugin_dir_url( __FILE__ ) );
+}
+
+require_once PLAINMARK_CORE_DIR . 'includes/custom-post-types.php';
+
+/**
+ * Flush rewrite rules when the core plugin is activated.
+ */
+function plainmark_core_activate() {
+	if ( function_exists( 'plainmark_register_portfolio_post_type' ) ) {
+		plainmark_register_portfolio_post_type();
+	}
+
+	if ( function_exists( 'plainmark_register_portfolio_taxonomy' ) ) {
+		plainmark_register_portfolio_taxonomy();
+	}
+
+	if ( function_exists( 'plainmark_register_technology_taxonomy' ) ) {
+		plainmark_register_technology_taxonomy();
+	}
+
+	flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'plainmark_core_activate' );
+
+/**
+ * Flush rewrite rules when the core plugin is deactivated.
+ */
+function plainmark_core_deactivate() {
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'plainmark_core_deactivate' );
